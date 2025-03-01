1ebe9b23cddd4e1870e1ca8b72ad6bcf30c80bd4
abdelmouiz <a_youb@estin.dz>
2025-03-01 10:50:05 -1100

    nothing but push

--- Server-Side/App.js
+++ Server-Side/App.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
 const app = express();
 const port = 3000
 app.use(express.json());
